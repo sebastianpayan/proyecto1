9f13728c6eda50e5dbcef7b6e58105ed829e2b4c
sebastianpayan <sebastianpayan908@gmail.com>
2023-08-31 22:04:54 +0200

    PROYECTO 1

---
+++ indexproject.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- Head section content here (meta tags, title, stylesheets, etc.) -->
+</head>
+
+<body>
+    <!-- Your website content here -->
+
+    <!-- Navbar -->
+    <?php include 'navbar.php'; ?>
+
+    <!-- Carousel -->
+    <?php include 'carousel.php'; ?>
+
+    <!-- Check Out Our Menu -->
+    <?php include 'menu.php'; ?>
+
+    <!-- Collapsible Menu Content -->
+    <?php include 'menu_collapsible.php'; ?>
+
+    <!-- Prices Lists -->
+    <?php include 'prices_lists.php'; ?>
+
+    <!-- Contact Us Section -->
+    <?php include 'contact.php'; ?>
+
+    <!-- Delivery Section -->
+    <?php include 'delivery.php'; ?>
+
+    <!-- Family Gathering Quote -->
+    <?php include 'family_quote.php'; ?>
+
+    <!-- Footer -->
+    <?php include 'footer.php'; ?>
+
+    <!-- Scripts and JavaScript files -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
